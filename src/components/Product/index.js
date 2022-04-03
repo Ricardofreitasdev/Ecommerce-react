313f98045e7d8b0335013ad0f83a2a43cffe2706
Ricardo Freitas <ricardofreitas07@live.com>
2022-04-03 20:33:53 -0300

    update component product

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,29 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function Product({ id, name, image, price, promotionalPrice }) {
-  const [add, setadd] = useState([]);
-
-  function addToCard(productId) {
-    setadd(productId);
-  }
-
+export default function Product({
+  id,
+  name,
+  image,
+  price,
+  promotionalPrice,
+  hot,
+  freeShipping,
+  discount,
+  payment,
+}) {
   return (
     <div className="product" data-product-id={id}>
-      <img className="product__image" loading="lazy" alt={name} src={image} />
-      <h3 className="product__name">{name}</h3>
-      <span className="product__price--original">de R${price}</span>
-      <span className="product__price--promotional">
-        por R$ {promotionalPrice}
-      </span>
-      <button
-        className="product__button"
-        type="button"
-        onClick={() => addToCard(id, promotionalPrice)}
-      >
+      {discount && <div className="product__discount">{discount}</div>}
+      <div className="product__wrapper">
+        <img className="product__image" loading="lazy" alt={name} src={image} />
+        <div className="product__label">
+          {hot !== 0 && <p className="product__label--hot">Lançamento</p>}
+          {freeShipping !== 0 && (
+            <p className="product__label--shipping">Frete Grátis</p>
+          )}
+        </div>
+      </div>
+      <div className="product__infos">
+        <h3 className="product__name">{name}</h3>
+        <div className="product__price">
+          <span className="product__price--original">de R${price}</span>
+          <span className="product__price--promotional">
+            <b>por R$ {promotionalPrice}</b>
+          </span>
+          {payment.map((item) => (
+            <p className="product__price--installments">
+              {item.plots}X de R${item.value} {item.taxes}
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <button className="product__spy" type="button">
+        espiar
+      </button>
+      <button className="product__button" type="button">
         Adicionar a sacola
       </button>
-
-      <p>{add}</p>
     </div>
   );
 }
